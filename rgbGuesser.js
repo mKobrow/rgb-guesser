9d585ccc979685ff0ec839800ce8ad077d97322e
NICHOLAS ROHRER <nicholasjrohrer@gmail.com>
2017-07-19 16:01:33 -0500

    styling

--- rgbGuesser.js
+++ rgbGuesser.js
@@ -54,7 +54,7 @@ resetButton.addEventListener("click", function(){
 		squares[i].style.background = colors[i];
 	}
 
-	h1.style.backgroundColor = "#232323";
+	h1.style.backgroundColor = "steelblue";
 });
 
 for (var i = 0; i < squares.length; i++){
